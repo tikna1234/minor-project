36a6d516f61f39ab6e087ba1c1e66f184035bdb9
db099 <dibyabharat2002@gmail.com>
2023-10-22 21:23:45 +0530

    Update random generator.py

--- random generator.py
+++ random generator.py
@@ -5,19 +5,20 @@ import random
 subjects = ["English", "Mathematics", "Science", "Social Studies", "Logical Reasoning", "Computer Awareness"]
 
 # Create a CSV file and write the header
-with open("student_marksheet2v.csv", mode='w', newline='') as file:
+with open("Vocational_marks.csv", mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Student ID",] + subjects)
 
     # Generate random data for 150 students
-    for student_id in range(1, 10001):
-        num = random.randint(1,10)
+    for student_id in range(6501, 10001):
+        marks = [random.randint(40, 54) for _ in subjects]
+        """num = random.randint(1,10)
         if(num <= 2):
             marks = [random.randint(80, 95) for _ in subjects]
         elif(num > 2 and num < 7):
             marks = [random.randint(65, 85) for _ in subjects]
         else:
-            marks = [random.randint(40, 65) for _ in subjects]
+            marks = [random.randint(40, 65) for _ in subjects]"""
         
         writer.writerow([student_id] + marks)
 
