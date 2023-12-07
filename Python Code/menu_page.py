bca14e15a4139d7ab4ec4d951bbfb13fa0811a33
cokwnker <ankitalex010@gmail.com>
2023-12-07 13:20:12 +0530

    further updates to code

--- Python Code/menu_page.py
+++ Python Code/menu_page.py
@@ -416,7 +416,7 @@ class Ui_MenuWindow(object):
     
     def Gencourses(self):
         english, mathematics, science, social_studies, logical_reasoning, computer, interests = self.fetchallsubjects()
-        avg = (english+mathematics+science+social_studies+logical_reasoning+computer)/6*100
+        avg = (english+mathematics+science+social_studies+logical_reasoning+computer)/6
         if avg>=75:
             branch = 0
         elif avg>=55:
@@ -425,75 +425,57 @@ class Ui_MenuWindow(object):
             branch = 2
         recommend = Printpred(english, mathematics, science, social_studies, logical_reasoning, computer, branch)
         courses = []
-        if branch == 0:
+        if branch == 0 or "(Diploma)" in recommend:
             if recommend == "Computer Science and Information Technology (Diploma)":
-                for i in self.diploma['Computer Science and Information Technology (Diploma)']:
-                    courses.append(i)
+                courses = diploma.get('Computer Science and Information Technology (Diploma)', [])
             elif recommend == "Mechanical and Electrical (Diploma)":
-                for i in self.diploma['Mechanical and Electrical (Diploma)']:
-                    courses.append(i)
+                courses = diploma.get('Mechanical and Electrical (Diploma)', [])
             elif recommend == "Electronics and Communication (Diploma)":
-                courses = str(self.diploma['Electronics and Communication (Diploma)'])
+                courses = diploma.get('Electronics and Communication (Diploma)', [])
             elif recommend == "Construction and Design (Diploma)":
-                for i in self.diploma['Construction and Design (Diploma)']:
-                    courses.append(i)
+                courses = diploma.get('Construction and Design (Diploma)', [])
             elif recommend == "Hospitality and Event Management (Diploma)":
-                for i in self.diploma['Hospitality and Event Management (Diploma)']:
-                    courses.append(i)
+                courses = diploma.get('Hospitality and Event Management (Diploma)', [])
             elif recommend == "Life Sciences and Environment (Diploma)":
-                for i in self.diploma['Life Sciences and Environment (Diploma)']:
-                    courses.append(i)
+                courses = diploma.get('Life Sciences and Environment (Diploma)', [])
             elif recommend == "Arts and Media (Diploma)":
-                for i in self.diploma['Arts and Media (Diploma)']:
-                    courses.append(i)
+                courses = diploma.get('Arts and Media (Diploma)', [])
             elif recommend == "Physical Education and Wellness (Diploma)":
-                for i in self.diploma['Physical Education and Wellness (Diploma)']:
-                    courses.append(i)
+                courses = diploma.get('Physical Education and Wellness (Diploma)', [])
             elif recommend == "Finance, Business and Marketing (Diploma)":
-                courses = self.diploma['Finance, Business and Marketing (Diploma)']
+                courses = diploma.get('Finance, Business and Marketing (Diploma)', [])
 
-        elif branch == 1:
+        elif branch == 1 or "(ITI)" in recommend:
             if recommend == "Computer Science and Information Technology (ITI)":
-                for i in self.iti['Computer Science and Information Technology (ITI)']:
-                    courses.append(i)
+                courses = iti.get('Computer Science and Information Technology (ITI)', [])
             elif recommend == "Mechanical and Electrical (ITI)":
-                for i in self.iti['Mechanical and Electrical (ITI)']:
-                    courses.append(i)
+                courses = iti.get('Mechanical and Electrical (ITI)', [])
             elif recommend == "Electronics and Communication (ITI)":
-                for i in self.iti['Electronics and Communication (ITI)']:
-                    courses.append(i)
+                courses = iti.get('Electronics and Communication (ITI)', [])
             elif recommend == "Construction and Design (ITI)":
-                for i in self.iti['Construction and Design (ITI)']:
-                    courses.append(i)
+                courses = iti.get('Construction and Design (ITI)', [])
             elif recommend == "Physical Education and Wellness (ITI)":
-                courses = str(self.iti['Physical Education and Wellness (ITI)'])
+                courses = iti.get('Physical Education and Wellness (ITI)', [])
 
         else:
             if recommend == "Computer Science and Information Technology (Vocational)":
-                for i in self.vocational['Computer Science and Information Technology (Vocational)']:
-                    courses.append(i)
+                courses = vocational.get('Computer Science and Information Technology (Vocational)', [])
             elif recommend == "Mechanical and Electrical (Vocational)":
-                for i in self.vocational['Mechanical and Electrical (Vocational)']:
-                    courses.append(i)
+                courses = vocational.get('Mechanical and Electrical (Vocational)', [])
             elif recommend == "Construction and Design (Vocational)":
-                for i in self.vocational['Construction and Design (Vocational)']:
-                    courses.append(i)
+                courses = vocational.get('Construction and Design (Vocational)', [])
             elif recommend == "Hospitality and Event Management (Vocational)":
-                for i in self.vocational['Hospitality and Event Management (Vocational)']:
-                    courses.append(i)
+                courses = vocational.get('Hospitality and Event Management (Vocational)', [])
             elif recommend == "Arts and Media (Vocational)":
-                for i in self.vocational['Arts and Media (Vocational)']:
-                    courses.append(i)
+                courses = vocational.get('Arts and Media (Vocational)', [])
             elif recommend == "Physical Education and Wellness (Vocational)":
-                courses = str(self.vocational['Physical Education and Wellness (Vocational)'])
+                courses = vocational.get('Physical Education and Wellness (Vocational)', [])
             elif recommend == "Finance, Business and Marketing (Vocational)":
-                for i in self.vocational['Finance, Business and Marketing (Vocational)']:
-                    courses.append(i)
+                courses = vocational.get('Finance, Business and Marketing (Vocational)', [])
             elif recommend == "Culinary Studies and Cooking (Vocational)":
-                for i in self.vocational['Culinary Studies and Cooking (Vocational)']:
-                    courses.append(i)
+                courses = vocational.get('Culinary Studies and Cooking (Vocational)', [])
                     
-        return str(recommend), courses, branch
+        return recommend, courses, branch
 
     def CheckRequirementsITI(self,interest,subs):
         weaksubs = []
